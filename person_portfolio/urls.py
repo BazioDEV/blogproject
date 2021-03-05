ec7c1bc0fbf04a6adb669795f3d4e694cd48a5ab
Pongsin Pongwachirint <bazio.developer@gmail.com>
2021-03-06 04:48:40 +0700

    update

--- person_portfolio/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register/', views.sign_up, name="user_sign"),
-    path('dashboard/', views.user_dashboard, name='user_dashboard'),
-    path('login/', views.sign_in, name='login_user'),
-]
